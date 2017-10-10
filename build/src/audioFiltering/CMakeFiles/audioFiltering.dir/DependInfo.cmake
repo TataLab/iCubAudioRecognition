018dc918ea1e528e1479d71b39310a1cfa818558
lukeinator42 <lukasgrasse@gmail.com>
2017-10-10 17:33:09 -0600

    init

---
+++ build/src/audioFiltering/CMakeFiles/audioFiltering.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/neuromorphic/data/iCubAudioRecognition/src/audioFiltering/src/audio_filtering.cpp" "/media/neuromorphic/data/iCubAudioRecognition/build/src/audioFiltering/CMakeFiles/audioFiltering.dir/src/audio_filtering.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "WEBRTC_AUDIO_PROCESSING_ONLY_BUILD=1"
+  "WEBRTC_POSIX=1"
+  "_REENTRANT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/include"
+  "/usr/local/include/webrtc_audio_processing"
+  "../src/audioFiltering/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/PocketsphinxSpeechRecognizer/build/CMakeFiles/speech_recognizer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/speech_recognizer.dir/src/speech_recognizer.cpp.o"
+  "speech_recognizer.pdb"
+  "speech_recognizer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/speech_recognizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
